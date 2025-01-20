6e9192ba97d799b3018b8c3ac2f88e9709cd82b7
Sandesh-Pahari <sandeshpahari05@gmail.com>
2025-01-20 10:20:21 +0545

    faq overlapping error solved whole page newly written

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::middleware('auth')->group(function () {
 Route::get('/new', function () {
     return view('frontend.home.new');
 });
+Route::get('/testtrekking', function () {
+    return view('frontend.trekking.testtrekking');
+});
 Route::get('/new2', function () {
     return view('frontend.home.new2');
 });
