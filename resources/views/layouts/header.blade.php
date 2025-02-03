7897ee0c3e916ac18032f7fcb12f21e953459030
Sandesh-Pahari <sandeshpahari05@gmail.com>
2025-02-03 22:39:02 +0545

    language button inserted into mobile view navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -264,7 +264,7 @@
                     <a href="{{ route('region') }}">
                         <button
                             class="flex items-center text-white font-bold px-3 py-2 hover:text-orange-400 focus:outline-none">
-                            Tours
+                            Tours 
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
@@ -644,7 +644,7 @@
                 </li>
 
                 <li class="relative group">
-                    <div class="language-selector text-gray-800 pl-1 py-3 rounded-md text-sm">
+                    <div  class="language-selector text-gray-800 pl-1 py-3 rounded-md text-sm">
                         <div id="gt-mordadam-43217984"></div>
                     </div>
                 </li>
@@ -669,17 +669,32 @@
 
 
 </nav>
+  
+<!-- Top Right Controls Container -->
+<div class="fixed top-4 right-4 z-30 flex items-center space-x-2 lg:hidden">
+    <!-- Language Selector -->
+    <div class="language-selector text-white p-2 rounded-md text-sm">
+        <div id="gt-mordadam-43217984"></div>
+    </div>
 
+    <!-- Close Button (hidden by default) -->
+    <button id="closeMobileMenu" 
+            class="text-white bg-blue-900 p-2 rounded-md focus:outline-none hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" 
+             stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </button>
 
-<!-- Hamburger Button (Visible only on smaller screens) -->
-<button id="mobileMenuButton"
-    class="fixed top-4 right-4 z-20 text-white bg-blue-900 p-2 rounded-md focus:outline-none lg:hidden">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-        stroke="currentColor" stroke-width="2">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
-    </svg>
-</button>
-
+    <!-- Hamburger Button (visible by default) -->
+    <button id="mobileMenuButton" 
+            class="text-white bg-blue-900 p-2 rounded-md focus:outline-none">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" 
+             stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+        </svg>
+    </button>
+</div>
 <!-- Mobile Navbar (Visible only on smaller screens) -->
 <div id="mobileNavbar"
     class="fixed top-0 right-0 w-full sm:w-96 h-full bg-blue-900 text-white transform translate-x-full transition-transform duration-300 z-10 lg:hidden  overflow-y-auto">
