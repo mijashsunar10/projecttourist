85479bfbd600aadcd3e78b0f08d3c7949e338786
Sandesh Pahari <sandeshpahari05@gmail.com>
2025-02-11 12:47:05 +0545

    gallery auth and change on nav bars in index pg

--- routes/web.php
+++ routes/web.php
@@ -1,144 +1,141 @@
     <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\CustomizeController;
-use App\Http\Controllers\FaqController;
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\InclusionExclusionController;
-use App\Http\Controllers\ItineraryController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RegionController;
-use App\Http\Controllers\RequiredItemController;
-use App\Http\Controllers\TrekController;
-use App\Http\Controllers\TripController;
-use App\Http\Controllers\TripDescriptionController;
-use App\Http\Controllers\TripFactController;
-use App\Http\Controllers\TripfaqController;
-use App\Http\Controllers\TripHighlightController;
-use App\Models\InclusionExclusion;
-use App\Models\TripDescription;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AdminController;
+    use App\Http\Controllers\BlogController;
+    use App\Http\Controllers\ContactController;
+    use App\Http\Controllers\CustomizeController;
+    use App\Http\Controllers\FaqController;
+    use App\Http\Controllers\GalleryController;
+    use App\Http\Controllers\InclusionExclusionController;
+    use App\Http\Controllers\ItineraryController;
+    use App\Http\Controllers\NewsController;
+    use App\Http\Controllers\ProfileController;
+    use App\Http\Controllers\RegionController;
+    use App\Http\Controllers\RequiredItemController;
+    use App\Http\Controllers\TrekController;
+    use App\Http\Controllers\TripController;
+    use App\Http\Controllers\TripDescriptionController;
+    use App\Http\Controllers\TripFactController;
+    use App\Http\Controllers\TripfaqController;
+    use App\Http\Controllers\TripHighlightController;
+    use App\Models\InclusionExclusion;
+    use App\Models\TripDescription;
+    use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('frontend.home.homepage');
-// })->name('index');
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/admindashboard',[AdminController::class,'dashboard'])->middleware(['auth', 'verified'])->name('admindashboard');
-
-Route::get('/new', function () {
-    return view('frontend.home.new');
-});
-Route::get('/new2', function () {
-    return view('frontend.home.new2');
-});
-
-// HomeCOntrolller
-
-// HomeCOntrolller
-
-// TrekController
-
-
-Route::get('/',[TrekController::class,'index'])->name('index');
-
-Route::get('/blog',[TrekController::class,'blog'])->name('blog');
-
-// Route::get('/news',[TrekController::class,'news'])->name('news');
-
-Route::get('/testimonials',[TrekController::class,'testimonials'])->name('testimonials');
-
-Route::get('/faq',[TrekController::class,'faq'])->name('faq');
-
-
-Route::get('/faq',[TrekController::class,'faq'])->name('faq');
-
-Route::get('/region',[TrekController::class,'region'])->name('region');
-
-Route::get('/trekinfo',[TrekController::class,'trekinfo'])->name('trekinfo');
-
-Route::get('/trek/main',[TrekController::class,'trekmain'])->name('trekmain');
-Route::get('/trek/main1',[TrekController::class,'trekmain1'])->name('trekmain1');
-
-
-Route::get('/gallerys',[TrekController::class,'gallery'])->name('gallerys');
+    // Route::get('/', function () {
+    //     return view('frontend.home.homepage');
+    // })->name('index');
 
 
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/regions/create', [RegionController::class, 'regionscreate'])->name('regionscreate');
-
-Route::post('/regionsstore', [RegionController::class, 'regionsstore'])->name('regionsstore');
-// TrekCOntroller
-
-// Route::controller(RegionController::class)->group(function () {
-//     Route::get('/regions', 'index')->middleware(['auth', 'verified'])->name('regionsindex');
-//     Route::get('/regions/create', 'regionscreate')->middleware(['auth', 'verified'])->name('regionscreate');
-//     Route::post('/regionsstore', 'regionsstore')->middleware(['auth', 'verified'])->name('regionsstore');
-//     Route::get('/regions/{id}/edit','regionsedit')->middleware(['auth', 'verified'])->name('regionsedit');
-//     Route::post('/regions/{id}/update', 'regionsupdate')->middleware(['auth', 'verified'])->name('regionsupdate');
-//     Route::post('/regions/{id}/delete',  'regionsdestroy')->middleware(['auth', 'verified'])->name('regionsdestroy');
-
-//     Route::get('/regions/{id}',  'regionshow')->middleware(['auth', 'verified'])->name('regionsshow');
-//     // Route::get('/editnews/{slug}', 'edit')->name('editnews');
-   
-// });
-
-Route::controller(RegionController::class)->group(function () {
-    Route::get('/regions', 'index')->name('regionsindex'); // Public
-    // Route::get('/header', 'header')->name('header'); // Public
-    Route::get('/regions/{id}',  'regionshow')->name('regionsshow'); // Public
-
-    Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('/regions/create', 'regionscreate')->name('regionscreate');
-        Route::post('/regionsstore', 'regionsstore')->name('regionsstore');
-        Route::get('/regions/{id}/edit','regionsedit')->name('regionsedit');
-        Route::post('/regions/{id}/update', 'regionsupdate')->name('regionsupdate');
-        Route::post('/regions/{id}/delete',  'regionsdestroy')->name('regionsdestroy');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
-});
+
+    Route::get('/admindashboard', [AdminController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('admindashboard');
+
+    Route::get('/new', function () {
+        return view('frontend.home.new');
+    });
+    Route::get('/new2', function () {
+        return view('frontend.home.new2');
+    });
+
+    // HomeCOntrolller
+
+    // HomeCOntrolller
+
+    // TrekController
 
 
-// Route::get('/userregions', [RegionController::class, 'userindex'])->name('userregions');
-// Route::get('/userregions/{id}', [RegionController::class,  'userregionshow'])->name('userregionsshow');
+    Route::get('/', [TrekController::class, 'index'])->name('index');
+
+    Route::get('/blog', [TrekController::class, 'blog'])->name('blog');
+
+    // Route::get('/news',[TrekController::class,'news'])->name('news');
+
+    Route::get('/testimonials', [TrekController::class, 'testimonials'])->name('testimonials');
+
+    Route::get('/faq', [TrekController::class, 'faq'])->name('faq');
 
 
-Route::controller(TripController::class)->group(function () {
+    Route::get('/faq', [TrekController::class, 'faq'])->name('faq');
 
-    Route::get('/trips/{id}', 'tripShow')->name('tripshow');
+    Route::get('/region', [TrekController::class, 'region'])->name('region');
 
-    Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/trekinfo', [TrekController::class, 'trekinfo'])->name('trekinfo');
 
-        Route::get('/regions/{region_id}/trips/create', 'tripscreate')->name('tripscreate');
-        Route::post('/regions/{region_id}/trips',  'tripsstore')->name('tripsstore');
-        Route::get('/trips/{id}/edit',  'tripsedit')->name('tripsedit');
-        Route::put('/trips/{id}', 'tripsupdate')->name('tripsupdate');
-        Route::delete('/trips/{id}',  'tripsdestroy')->name('tripsdestroy');
+    Route::get('/trek/main', [TrekController::class, 'trekmain'])->name('trekmain');
+    Route::get('/trek/main1', [TrekController::class, 'trekmain1'])->name('trekmain1');
+
+
+    Route::get('/gallerys', [TrekController::class, 'gallery'])->name('gallerys');
+
+
+
+
+    Route::get('/regions/create', [RegionController::class, 'regionscreate'])->name('regionscreate');
+
+    Route::post('/regionsstore', [RegionController::class, 'regionsstore'])->name('regionsstore');
+    // TrekCOntroller
+
+    // Route::controller(RegionController::class)->group(function () {
+    //     Route::get('/regions', 'index')->middleware(['auth', 'verified'])->name('regionsindex');
+    //     Route::get('/regions/create', 'regionscreate')->middleware(['auth', 'verified'])->name('regionscreate');
+    //     Route::post('/regionsstore', 'regionsstore')->middleware(['auth', 'verified'])->name('regionsstore');
+    //     Route::get('/regions/{id}/edit','regionsedit')->middleware(['auth', 'verified'])->name('regionsedit');
+    //     Route::post('/regions/{id}/update', 'regionsupdate')->middleware(['auth', 'verified'])->name('regionsupdate');
+    //     Route::post('/regions/{id}/delete',  'regionsdestroy')->middleware(['auth', 'verified'])->name('regionsdestroy');
+
+    //     Route::get('/regions/{id}',  'regionshow')->middleware(['auth', 'verified'])->name('regionsshow');
+    //     // Route::get('/editnews/{slug}', 'edit')->name('editnews');
+
+    // });
+
+    Route::controller(RegionController::class)->group(function () {
+        Route::get('/regions', 'index')->name('regionsindex'); // Public
+        // Route::get('/header', 'header')->name('header'); // Public
+        Route::get('/regions/{id}',  'regionshow')->name('regionsshow'); // Public
+
+        Route::middleware(['auth', 'verified'])->group(function () {
+            Route::get('/regions/create', 'regionscreate')->name('regionscreate');
+            Route::post('/regionsstore', 'regionsstore')->name('regionsstore');
+            Route::get('/regions/{id}/edit', 'regionsedit')->name('regionsedit');
+            Route::post('/regions/{id}/update', 'regionsupdate')->name('regionsupdate');
+            Route::post('/regions/{id}/delete',  'regionsdestroy')->name('regionsdestroy');
         });
-    
+    });
 
-});
 
-    Route::controller(TripDescriptionController::class)->middleware(['auth', 'verified'])->group(function()
-    {
+    // Route::get('/userregions', [RegionController::class, 'userindex'])->name('userregions');
+    // Route::get('/userregions/{id}', [RegionController::class,  'userregionshow'])->name('userregionsshow');
 
-     Route::post('/trips/{id}/add-images',  'addImages')->name('addtripimages');
-    Route::post('/images/{id}/update',  'updateImage')->name('updateimage');
-    Route::delete('/images/{id}/delete', 'deleteImage')->name('deleteimage');
+
+    Route::controller(TripController::class)->group(function () {
+
+        Route::get('/trips/{id}', 'tripShow')->name('tripshow');
+
+        Route::middleware(['auth', 'verified'])->group(function () {
+
+            Route::get('/regions/{region_id}/trips/create', 'tripscreate')->name('tripscreate');
+            Route::post('/regions/{region_id}/trips',  'tripsstore')->name('tripsstore');
+            Route::get('/trips/{id}/edit',  'tripsedit')->name('tripsedit');
+            Route::put('/trips/{id}', 'tripsupdate')->name('tripsupdate');
+            Route::delete('/trips/{id}',  'tripsdestroy')->name('tripsdestroy');
+        });
+    });
+
+    Route::controller(TripDescriptionController::class)->middleware(['auth', 'verified'])->group(function () {
+
+        Route::post('/trips/{id}/add-images',  'addImages')->name('addtripimages');
+        Route::post('/images/{id}/update',  'updateImage')->name('updateimage');
+        Route::delete('/images/{id}/delete', 'deleteImage')->name('deleteimage');
     });
 
     Route::prefix('trips/{trip_id}/highlights')->group(function () {
@@ -148,7 +145,6 @@ Route::controller(TripController::class)->group(function () {
         Route::get('/edit', [TripHighlightController::class, 'edit'])->name('tripHighlightsedit');
         Route::post('/update', [TripHighlightController::class, 'update'])->name('tripHighlightsupdate');
         Route::delete('/delete', [TripHighlightController::class, 'destroy'])->name('tripHighlightsdestroy');
-
     });
 
     Route::prefix('trips/{trip_id}/itinerary')->group(function () {
@@ -168,11 +164,11 @@ Route::controller(TripController::class)->group(function () {
 
 
     Route::get('trips/{trip_id}/trip-facts/create', [TripFactController::class, 'create'])->name('tripfactcreate');
-Route::post('trips/{trip_id}/trip-facts/store', [TripFactController::class, 'store'])->name('tripfactstore');
-Route::get('trips/{trip_id}/trip-facts/{fact_id}/edit', [TripFactController::class, 'edit'])->name('tripfactedit');
-Route::post('trips/{trip_id}/trip-facts/{fact_id}/update', [TripFactController::class, 'update'])->name('tripfactupdate');
-Route::delete('trips/{trip_id}/trip-facts/{fact_id}', [TripFactController::class, 'destroy'])->name('tripfactdestroy');     
-    
+    Route::post('trips/{trip_id}/trip-facts/store', [TripFactController::class, 'store'])->name('tripfactstore');
+    Route::get('trips/{trip_id}/trip-facts/{fact_id}/edit', [TripFactController::class, 'edit'])->name('tripfactedit');
+    Route::post('trips/{trip_id}/trip-facts/{fact_id}/update', [TripFactController::class, 'update'])->name('tripfactupdate');
+    Route::delete('trips/{trip_id}/trip-facts/{fact_id}', [TripFactController::class, 'destroy'])->name('tripfactdestroy');
+
     Route::controller(NewsController::class)->group(function () {
         Route::get('/news', 'index')->name('news');
         Route::get('/addnews', 'create')->name('createnews');
@@ -182,23 +178,23 @@ Route::delete('trips/{trip_id}/trip-facts/{fact_id}', [TripFactController::class
         Route::delete('/delete/{slug}', 'destroy')->name('deletenews');
     });
 
-    Route::get('/contact',[TrekController::class,'contact'])->name('contact');
+    Route::get('/contact', [TrekController::class, 'contact'])->name('contact');
     Route::post('/contact/send', [ContactController::class, 'submitContactForm'])->name('contact.send');
 
 
     // Route::resource('/faqs', FaqController::class);
     Route::get('faqs', [FaqController::class, 'index'])->name('faqs.index'); // Publicly accessible
     Route::middleware(['auth'])->group(function () {
-    
-    Route::get('faqs/create', [FaqController::class, 'create'])->name('faqs.create');
-    Route::post('faqs', [FaqController::class, 'store'])->name('faqs.store');
-    Route::get('faqs/{faq:slug}/edit', [FaqController::class, 'edit'])->name('faqs.edit');
-    Route::put('faqs/{faq:slug}', [FaqController::class, 'update'])->name('faqs.update');
-    Route::delete('faqs/{faq:slug}', [FaqController::class, 'destroy'])->name('faqs.destroy');
-});
+
+        Route::get('faqs/create', [FaqController::class, 'create'])->name('faqs.create');
+        Route::post('faqs', [FaqController::class, 'store'])->name('faqs.store');
+        Route::get('faqs/{faq:slug}/edit', [FaqController::class, 'edit'])->name('faqs.edit');
+        Route::put('faqs/{faq:slug}', [FaqController::class, 'update'])->name('faqs.update');
+        Route::delete('faqs/{faq:slug}', [FaqController::class, 'destroy'])->name('faqs.destroy');
+    });
 
 
-    Route::get('/customize',[TrekController::class,'customize'])->name('customize');    
+    Route::get('/customize', [TrekController::class, 'customize'])->name('customize');
     Route::post('/customize/send', [CustomizeController::class, 'submitCustomizeForm'])->name('customize.send');
 
 
@@ -210,35 +206,38 @@ Route::delete('trips/{trip_id}/trip-facts/{fact_id}', [TripFactController::class
         Route::put('/blogs/{slug}/update', 'update')->name('blogs.update');
         Route::delete('/blogs/{id}/destroy', 'destroy')->name('blogs.destroy');
         Route::get('/blogs/{slug}/{id}/show', 'show')->name('blogs.show');
-    });  
-    
-    
-    Route::resource('gallery', GalleryController::class);
+    });
+
+    Route::get('gallery', [GalleryController::class, 'index'])->name('gallery.index');
+
+    Route::middleware(['auth'])->group(function () {
+        Route::resource('gallery', GalleryController::class)->except(['index']);
+    });
 
 
     Route::get('/trip/{trip_id}/requireditems/create', [RequiredItemController::class, 'create'])->name('requireditems.create');
-Route::post('/trip/{trip_id}/requireditems/store', [RequiredItemController::class, 'store'])->name('requireditems.store');
-Route::get('/trip/{trip_id}/requireditems/{id}/edit', [RequiredItemController::class, 'edit'])->name('requireditems.edit');
-Route::put('/trip/{trip_id}/requireditems/{id}/update', [RequiredItemController::class, 'update'])->name('requireditems.update');
-Route::delete('/trip/{trip_id}/requireditems/{id}/delete', [RequiredItemController::class, 'destroy'])->name('requireditems.destroy');
+    Route::post('/trip/{trip_id}/requireditems/store', [RequiredItemController::class, 'store'])->name('requireditems.store');
+    Route::get('/trip/{trip_id}/requireditems/{id}/edit', [RequiredItemController::class, 'edit'])->name('requireditems.edit');
+    Route::put('/trip/{trip_id}/requireditems/{id}/update', [RequiredItemController::class, 'update'])->name('requireditems.update');
+    Route::delete('/trip/{trip_id}/requireditems/{id}/delete', [RequiredItemController::class, 'destroy'])->name('requireditems.destroy');
 
 
-// Show Create Form for Inclusions & Exclusions
-Route::get('trips/{trip}/inclusions-exclusions/create', [InclusionExclusionController::class, 'create'])->name('trips.inclusions-exclusions.create');
+    // Show Create Form for Inclusions & Exclusions
+    Route::get('trips/{trip}/inclusions-exclusions/create', [InclusionExclusionController::class, 'create'])->name('trips.inclusions-exclusions.create');
 
-// Store Inclusions & Exclusions
-Route::post('trips/{trip}/inclusions-exclusions', [InclusionExclusionController::class, 'store'])->name('trips.inclusions-exclusions.store');
+    // Store Inclusions & Exclusions
+    Route::post('trips/{trip}/inclusions-exclusions', [InclusionExclusionController::class, 'store'])->name('trips.inclusions-exclusions.store');
 
-// Edit Inclusion/Exclusion
-Route::get('trips/{trip}/inclusions-exclusions/{inclusionExclusion}/edit', [InclusionExclusionController::class, 'edit'])->name('trips.inclusions-exclusions.edit');
+    // Edit Inclusion/Exclusion
+    Route::get('trips/{trip}/inclusions-exclusions/{inclusionExclusion}/edit', [InclusionExclusionController::class, 'edit'])->name('trips.inclusions-exclusions.edit');
 
-// Update Inclusion/Exclusion
-Route::put('trips/{trip}/inclusions-exclusions/{inclusionExclusion}', [InclusionExclusionController::class, 'update'])->name('trips.inclusions-exclusions.update');
+    // Update Inclusion/Exclusion
+    Route::put('trips/{trip}/inclusions-exclusions/{inclusionExclusion}', [InclusionExclusionController::class, 'update'])->name('trips.inclusions-exclusions.update');
 
-// Delete Inclusion/Exclusion
-Route::delete('trips/{trip}/inclusions-exclusions/{inclusionExclusion}', [InclusionExclusionController::class, 'destroy'])->name('trips.inclusions-exclusions.destroy');
-
-
+    // Delete Inclusion/Exclusion
+    Route::delete('trips/{trip}/inclusions-exclusions/{inclusionExclusion}', [InclusionExclusionController::class, 'destroy'])->name('trips.inclusions-exclusions.destroy');
 
 
-require __DIR__.'/auth.php';
+
+
+    require __DIR__ . '/auth.php';
